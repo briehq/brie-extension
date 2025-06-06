9e7ab8e590e2d5c77eb85cfff96cbfe6d9ccd750
KUNAL KUMAR <kunaldevspro@gmail.com>
2025-06-06 21:02:14 +0530

    feature(172): added safari support

---
+++ Brie - Bug Capture/Brie - Bug Capture/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Brie - Bug Capture
+//
+//  Created by Kunal Kumar on 06/06/25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
