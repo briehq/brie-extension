2e988436b71909dc87937e088cf1e4bd5fbc0d29
Ion Leu <ion.leu@gmail.com>
2025-06-07 21:53:39 -0400

    chore: remove unused login guest stuff

--- pages/popup/src/popup-content.tsx
+++ pages/popup/src/popup-content.tsx
@@ -1,37 +1,19 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import { useStorage } from '@extension/shared';
-import { authTokensStorage, captureStateStorage, userUUIDStorage } from '@extension/storage';
-import { useLoginGuestMutation } from '@extension/store';
+import { captureStateStorage } from '@extension/storage';
 
 import { CaptureScreenshotGroup } from './components/capture';
 import { AuthGuard } from './components/guards';
 import { SlicesHistoryButton, SlicesHistoryContent } from './components/slices-history';
-import { Header, BetaNotifier, Skeleton } from './components/ui';
+import { Header, BetaNotifier } from './components/ui';
 
 export const PopupContent = () => {
-  const captureState = useStorage(captureStateStorage);
-  const tokens = useStorage(authTokensStorage);
-  const uuid = useStorage(userUUIDStorage);
-
   const [showSlicesHistory, setShowSlicesHistory] = useState(false);
-  // const [loginGuest, { isLoading }] = useLoginGuestMutation();
 
-  // useEffect(() => {
-  //   const initialGuestLogin = async () => {
-  //     if (!tokens?.accessToken && uuid) {
-  //       loginGuest({ uuid });
-  //     }
-  //   };
-
-  //   initialGuestLogin();
-  // }, [loginGuest, tokens?.accessToken, uuid]);
+  const captureState = useStorage(captureStateStorage);
 
   const handleOnBack = () => setShowSlicesHistory(false);
-
-  // if (isLoading) {
-  //   return <Skeleton />;
-  // }
 
   return (
     <AuthGuard>
