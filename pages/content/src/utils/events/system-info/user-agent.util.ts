32a84b139d2702f9248536a6b1da0b9b052fdb5b
vishal <vishalkumarvkvk988@gmail.com>
2025-06-13 11:25:53 +0530

    refactor: handle browser detection using userAgentData for Chromium and fallback for others
    - Use navigator.userAgentData.brands for Chromium-based browsers
    - Fallback to navigator.userAgent for non-Chromium (e.g., Safari, Firefox)
    - Removed default case as it's no longer needed
    
    Signed-off-by: vishal <vishalkumarvkvk988@gmail.com>

--- pages/content/src/utils/events/system-info/user-agent.util.ts
+++ pages/content/src/utils/events/system-info/user-agent.util.ts
@@ -6,37 +6,48 @@ import { getBrowserZoomLevel } from './zoom-level.util';
 /** Parses navigator.userAgent to extract browser and OS info. */
 export const parseUserAgent = (): { browser: BrowserInfo; os: OSInfo } => {
   const userAgent = navigator.userAgent.toLowerCase();
-
+  const uaData = (navigator as any).userAgentData || {};
+  const platform = userAgent || '';
   let browserName = 'Unknown';
   let browserVersion = 'Unknown';
   let osName = 'Unknown';
   let osVersion = 'Unknown';
 
-  // Browser detection
-  if (userAgent.includes('firefox') || userAgent.includes('Firefox')) {
-    browserName = 'Firefox';
-    browserVersion = userAgent.match(/firefox\/([\d.]+)/)?.[1] || 'Unknown';
-  } else if ((userAgent.includes('safari') || userAgent.includes('Safari')) && !userAgent.includes('chrome')) {
-    browserName = 'Safari';
-    browserVersion = userAgent.match(/version\/([\d.]+)/)?.[1] || 'Unknown';
-  } else if (userAgent.includes('Edg')) {
-    browserName = 'Edge';
-    browserVersion = userAgent.match(/edg\/([\d.]+)/)?.[1] || 'Unknown';
-  } else if (userAgent.includes('chrome') || userAgent.includes('Chrome')) {
-    browserName = 'Chrome';
-    browserVersion = userAgent.match(/Chrome\/(\d+)/i)?.[1] || 'Unknown';
-  } else if (userAgent.includes('Opera')) {
-    browserName = 'Opera';
-    browserVersion = userAgent.match(/opera\/([\d.]+)/)?.[1] || 'Unknown';
+  //   FIrst approach would be :
+  //   detect browser using browser name and browser version using uaData !
+  // uaData - for browser : Chrome, Edge, Opera, Dia, Brave, Samsung Internet as they are chromium based
+
+  const brands = uaData.brands;
+  browserName = brands[0].brand;
+  browserVersion = brands[0].version;
+
+  //  for non chromium based browser (Firefox / Safari) running on macos or on lInux
+  if (browserName === 'Unknown' && browserVersion === 'Unknown') {
+    // for firefox
+    if (userAgent.includes('firefox') || userAgent.includes('Firefox')) {
+      browserName = 'Firefox';
+      browserVersion = userAgent.match(/firefox\/([\d.]+)/)?.[1] || '';
+      const matchMac = userAgent.match(/Mac OS /i);
+      const matchLinux = userAgent.match(/Linux/i);
+      if (matchMac) {
+        osName = 'Mac OS';
+      } else if (matchLinux) {
+        osName = 'Linux';
+      }
+    }
+
+    // for safari
+    if ((userAgent.includes('safari') || userAgent.includes('Safari')) && !userAgent.includes('chrome')) {
+      browserName = 'Safari';
+      browserVersion = userAgent.match(/version\/([\d.]+)/)?.[1] || '';
+      osVersion = userAgent.match(/OS ([\d_]+)/i)?.[1]?.replace(/_/g, '.') || 'Unknown';
+      osName = 'Mac OS';
+    }
   }
 
-  const platform = userAgent || '';
-  if (platform.includes('mac')) {
-    osName = 'Mac OS';
-  } else if (platform.includes('win')) {
-    osName = 'Windows';
-    osVersion = userAgent.match(/Windows NT ([\d.]+)/)?.[1] || 'Unknown';
-  } else if (/iphone|ipad|ipod/.test(userAgent)) {
+  // now for mobile devices  based browsers like
+
+  if (/iphone|ipad|ipod/.test(userAgent)) {
     osName = 'iOS';
     osVersion = userAgent.match(/OS ([\d_]+)/i)?.[1]?.replace(/_/g, '.') || 'Unknown';
   } else if (platform.includes('android')) {
