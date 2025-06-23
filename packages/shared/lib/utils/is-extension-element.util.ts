981f5da5c1b888ba0df80ef373553958eb4cfb9d
rafaeljohn9 <rafaeljohb@gmail.com>
2025-06-23 21:59:07 +0300

    Fix: return condition
    Signed-off-by: rafaeljohn9 <rafaeljohb@gmail.com>

--- packages/shared/lib/utils/is-extension-element.util.ts
+++ packages/shared/lib/utils/is-extension-element.util.ts
@@ -1,10 +1,5 @@
 export const isExtensionElement = (element: HTMLElement): boolean => {
   if (!element) return false;
 
-  // Check for your specific extension container IDs
-  if (element.closest('#brie-root')) {
-    return true;
-  }
-
-  return false;
+  return element.closest('#brie-root') !== null;
 };
