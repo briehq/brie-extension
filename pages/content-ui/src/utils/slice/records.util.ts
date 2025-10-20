ae2845df175920aa6eb6d4df33d20a7b683cb990
Ion Leu <ion.leu@gmail.com>
2025-10-20 12:29:29 -0400

    bug(records): use url instead of pageurl to store current web location

--- pages/content-ui/src/utils/slice/records.util.ts
+++ pages/content-ui/src/utils/slice/records.util.ts
@@ -9,3 +9,15 @@ export const getRecords = () => {
     });
   });
 };
+
+export const deleteRecords = () => {
+  return new Promise((resolve, reject) => {
+    chrome.runtime.sendMessage({ type: 'DELETE_RECORDS' }, response => {
+      if (chrome.runtime.lastError) {
+        return reject(new Error(chrome.runtime.lastError.message));
+      }
+
+      resolve(response.records);
+    });
+  });
+};
